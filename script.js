ce42622d1a172dda9e1edf66ef695c7f5a42d599
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-12 15:34:20 -0800

    url up

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-var urlEl = $("url"); 
+fetch('www.themealdb.com/api/json/v1/1/random.php', {
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+
+
+
+
+  
