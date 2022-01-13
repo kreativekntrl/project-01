a53408b1020b532039477ccbcf939d1f663d0081
chrismuto <chrismuto@gmail.com>
2022-01-13 12:25:20 -0800

    converted javascript to jQuery

--- assets/js/youtube-api-js.js
+++ assets/js/youtube-api-js.js
@@ -1,10 +1,8 @@
-var test = document.getElementById("test");
-var fetchedURL = data.meals[0].strYoutube;
-var firstSplit = fetchedURL.split("watch");
-var secondSplit = firstSplit[1].split("=");
-console.log(firstSplit[0]);
-console.log(secondSplit[1]);
-videoURL = firstSplit[0] + "embed/" + secondSplit[1];
-console.log(videoURL);
-test.src = videoURL;
-  
+function displayVideo() {
+    var test = $("#test");
+    var fetchedURL = data.meals[0].strYoutube;
+    var firstSplit = fetchedURL.split("watch");
+    var secondSplit = firstSplit[1].split("=");
+    videoURL = firstSplit[0] + "embed/" + secondSplit[1];
+    test.attr("src", videoURL);
+  }
